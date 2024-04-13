3548db561e8c0933f548cc953b64fffa94256b58
darrk-heart <prospaul999@gmail.com>
2024-04-13 03:00:56 +0000

    working on the home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import bg from "../../assets/home/homebg.jpg";
 import styles from "./Home.module.css";
+import Heading from "../Head/Heading";
 
 function Home() {
   return (
     <div className={styles.container}>
       <img src={bg} alt="background" className={styles.back} />
+      <div className={styles.content}>
+        <Heading />
+      </div>
     </div>
   );
 }
