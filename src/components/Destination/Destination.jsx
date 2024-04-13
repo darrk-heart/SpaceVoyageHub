1ca5fbf6778f4e1f3fd9106759433ef4e354b76c
darrk-heart <prospaul999@gmail.com>
2024-04-13 07:12:09 +0000

    writing codes to fetch the data from the cache

--- src/components/Destination/Destination.jsx
+++ src/components/Destination/Destination.jsx
@@ -1,30 +1,28 @@
 import React from "react";
 import bg from "../../assets/destination/destinationbg.jpg";
 import styles from "./Destination.module.css";
+import data from "../../data/data.json";
 
 function Destination({ children }) {
+  const names = data.destinations.map((item) => item.name);
+
   return (
     <div className={styles.container}>
       <img src={bg} alt="background" className={styles.back} />
       <div>
         <div className={styles.heading}>
           {children}
-          <div>01 PICK YOUR DESTINATION</div>
           <div className={styles.half}>
             <div className={styles.halfOne}>
-              <h4>SO, YOU WANT TO TRAVEL TO </h4>
-              <h1>SPACE</h1>
-              <p>
-                Let's face it; if you want to go to space, you might as well
-                <br />
-                genuinely go to outer space and not hover kind of on the
-                <br /> edge of it. Well sit back, and relax because we'll give
-                you
-                <br /> a truly out of this world experience!
-              </p>
+              <span>01</span>
+              <span>PICK YOUR DESTINATION</span>
             </div>
             <div className={styles.halfTwo}>
-              <p>EXPLORE</p>
+              <ul>
+                {names.map((name, index) => (
+                  <li key={index}>{name}</li>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
