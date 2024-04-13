332b3c72f7f972dc423b2cbfd5c5ab4b643ea6ce
darrk-heart <prospaul999@gmail.com>
2024-04-13 05:56:14 +0000

    working on the home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,26 @@ function Home({ children }) {
     <div className={styles.container}>
       <img src={bg} alt="background" className={styles.back} />
       <div>
-        <div className={styles.heading}>{children}</div>
+        <div className={styles.heading}>
+          {children}{" "}
+          <div className={styles.half}>
+            <div className={styles.halfOne}>
+              <h4>SO, YOU WANT TO TRAVEL TO </h4>
+              <h1>SPACE</h1>
+              <p>
+                Let's face it; if you want to go to space, you might as well
+                <br />
+                genuinely go to outer space and not hover kind of on the
+                <br /> edge of it. Well sit back, and relax because we'll give
+                you
+                <br /> a truly out of this world experience!
+              </p>
+            </div>
+            <div className={styles.halfTwo}>
+              <p>EXPLORE</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
